95d13c55907ab9a08f9b47141f3d1fe6f61ace63
Victor <vcarvalho0402@gmail.com>
2019-12-28 12:49:32 -0300

    Add the folder documents to the FolderSerializer

--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 class FolderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :content, :descendants, :ancestors, :ancestry
-
-  def ancestors
-    @object.ancestors
-  end
-
-  def parents
-    @object.descendants
-  end
+  attributes :id, :name, :content, :descendants, :ancestors, :ancestry, :documents
 end
